1cd1e332d667b2a9cbbc6aad02e23f012dba9e87
Владислав Рыбачук <vlad1k-o@yandex.ru>
2022-08-12 19:37:21 +0300

    Текущее состояние проекта

---
+++ yatube/posts/views.py
@@ -0,0 +1,28 @@
+from multiprocessing import context
+from turtle import title
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post, Group 
+
+
+# General page
+def index(request):
+  # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
+
+# Page with  filtered posts
+def group_posts(request,slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
